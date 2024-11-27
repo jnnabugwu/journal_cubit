a3194c29a40414013cbf849da85cd19f2c7fe580
Jordan Nnabugwu <jordannnabugwu@gmail.com>
2024-11-26 20:46:25 -0600

    chore: getting to a good state

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import firebase_core
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
